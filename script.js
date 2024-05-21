fe627f066395bec4ae4357ed013d4c2d7b5c244f
Luiza <luiza.cristina@gmail.com>
2024-05-21 19:36:44 -0300

    feat: initializes free flow app

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const contentDiv = document.getElementById('content');
+
+  // Carregar o dashboard por padrão quando a página é carregada
+  loadContent('dashboard.html');
+
+  document
+    .getElementById('dashboardLink')
+    .addEventListener('click', function (event) {
+      event.preventDefault();
+      loadContent('dashboard.html');
+    });
+
+  document
+    .getElementById('cameraLink')
+    .addEventListener('click', function (event) {
+      event.preventDefault();
+      loadContent('camera.html');
+    });
+
+  function loadContent(page) {
+    fetch(page)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then((data) => {
+        contentDiv.innerHTML = data;
+      })
+      .catch((error) => {
+        console.error(
+          'There has been a problem with your fetch operation:',
+          error
+        );
+        contentDiv.innerHTML =
+          '<p>Sorry, there was an error loading the content.</p>';
+      });
+  }
+});
